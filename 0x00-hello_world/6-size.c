55a5b98d595d18c140b41d85c502ac894d404f71
redstorm-hub <nigussieabreham80@gmail.com>
2022-04-08 14:50:53 +0300

    quick fix on task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ float f;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(x));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(y));
-printf("Size of a long long int: %ul byte(s)\n", (unsigned long)sizeof(z));
-pritnf("Size of a float: %ul byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(z));
+pritnf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
