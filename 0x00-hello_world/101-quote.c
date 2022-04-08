4033629ef9ddf804f19573508420d7d1176d6fe0
redstorm-hub <nigussieabreham80@gmail.com>
2022-04-09 02:35:54 +0300

    applying betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2013-10-19\n", 59);
