3ebe9e66f02c2de8fb69edbe3490ff33c737d232
redstorm-hub <nigussieabreham80@gmail.com>
2022-04-08 17:19:58 +0300

    Fixing indentation

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,6 @@
 /**
  * Main function - Hundle the function body
  */
-
 int main(void)
 {
 	int n;
@@ -15,21 +14,24 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d", n);
+		printf(" is positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("%d", n);
+		printf(" is zero\n");
+	}
+	else
+	{
+		printf("%d", n);
+		printf(" is negative\n");
+	}
 
-if (n > 0)
-{
-printf("%d", n);
-printf(" is positive\n");
-}
-else if (n == 0)
-{
-printf("%d", n);
-printf(" is zero\n");
-}
-else
-{
-printf("%d", n);
-printf(" is negative\n");
-}
-return (0);
+	/**
+	  * return 0
+	  */
+	return (0);
 }
