17d09d0aae823d191a28111db1ff7711274380db
redstorm-hub <nigussieabreham80@gmail.com>
2022-04-13 00:27:51 +0300

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include <main.h>
 
 /**
@@ -8,7 +7,15 @@
   * Return: 0
   */
 
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
