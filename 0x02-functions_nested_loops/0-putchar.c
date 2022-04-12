728409f2d8950c07b1eaf43c16c1d004e7f856e0
redstorm-hub <nigussieabreham80@gmail.com>
2022-04-12 23:42:45 +0300

    Functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  *
+  * Description: Print _putchar, followed by new line
+  * Return: 0
+  */
+
+int main()
+{
+	printf("_putchar\n");
+	return (0);
+}
