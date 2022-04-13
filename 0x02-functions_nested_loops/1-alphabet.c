8b0ebc4c7980e1c1dfe13874106cb6310e754d9b
redstorm-hub <nigussieabreham80@gmail.com>
2022-04-13 20:40:45 +0300

    fixing betty coding style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * print_alphabet - Print the alphabet, in lowercase, followed by new line 
+  *print_alphabet - Print the alphabet, in lowercase, followed by new line 
  */
 
 void print_alphabet(void)
