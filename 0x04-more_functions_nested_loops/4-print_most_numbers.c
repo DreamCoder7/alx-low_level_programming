8b7ed3fffb32396a21ddc38ab93f9b4bb736db25
redstorm-hub <nigussieabreham80@gmail.com>
2022-04-15 19:15:41 +0300

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_most_numbers(void) : prints the number from 0 to 9
+  *
+  * Retrun: zero
+  */
+int print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		_putchar('%d' + num);
+	}
+
+	return (0);
+}
