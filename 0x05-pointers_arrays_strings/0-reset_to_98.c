c21a44756cee5d4760961139b769c5224df31342
redstorm-hub <nigussieabreham80@gmail.com>
2022-04-18 22:52:58 +0300

    quick fix on task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * reset_to_98 - Takes a pointer to an int as parameter and updat  *		    e the value it point to 98
+  * reset_to_98 - Takes a pointer to an int as parameter and updat  *		    e the value
   * @n : int parameter
   *
   * Return - nothing
