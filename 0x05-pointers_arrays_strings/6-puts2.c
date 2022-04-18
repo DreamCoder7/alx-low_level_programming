6f1e69adc89f1115f76f63d906ea6cc00cd2c79e
redstorm-hub <nigussieabreham80@gmail.com>
2022-04-19 00:48:13 +0300

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+/**
+  * puts2 - Prints every other character of a string
+  * @str: recive string
+  *
+  * Return: nothing
+  */
 void puts2(char *str)
 {
 	int i = 0;
+
 	while (str[i] != '\0')
 	{
 		if (i / 2 == 0)
