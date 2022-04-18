417feb7e94434d98f38bc5a18a13f8f3002a99cb
redstorm-hub <nigussieabreham80@gmail.com>
2022-04-19 00:39:06 +0300

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "_putchar.c"
+
+void puts2(char *str)
+{
+	while (s[i] != '\0')
+	{
+		if (i / 2 == 0)
+		{
+			_putchar(i);
+		}
+
+		_putchar('\n');
+	}
+
+}
---
+++ 0x05-pointers_arrays_strings/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "_putchar.c"
+
+
+int main(void)
+{
+	char *s;
+	char *str = "0123456";
+	int i = 0;
+
+	_putchar(str);
+	/*
+	while (s[i] != '\0')
+	{
+		if (i / 2 == 0)
+		{
+			_putchar(i);
+		}
+	}
+	*/
+
+}
