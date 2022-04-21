d82887510b50af7afc4b5969d874fb75aeca0a57
redstorm-hub <nigussieabreham80@gmail.com>
2022-04-21 19:27:49 +0300

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * *_strcat - function that concatenates two strings.
+  *
+  * @dest: pointer to destination input
+  * @src: pointer to resource input
+  *
+  * Return: pointer to resulting string @dest
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+	x = 0;
+
+	while (dest[x])
+	{
+		x++;
+	}
+
+	for (y = 0; src[y]; y++)
+	{
+		dest[x++] = src[y];
+		return (dest);
+	}
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+0x006. C - More pointers, arrays and strings
+
+0. strcat
+	- Function that concatenates two strings.
