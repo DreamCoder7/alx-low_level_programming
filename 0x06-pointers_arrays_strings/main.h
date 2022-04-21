7c99207e03b3ee351e619fc1c91f6bb3dca0b7ae
redstorm-hub <nigussieabreham80@gmail.com>
2022-04-21 19:34:27 +0300

    task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#include <stdio.h>
 
-char *_strcat(char *dest, char *src);
-#endif
+/**
+  * main - check the code
+  *
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+	char s1[98] = "Hello ";
+	char s2[] = "world!\n";
+	char *ptr;
+
+	printf("%s\n", s1);
+	printf("%s", s2);
+	ptr = _strcat(s1, s2);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
+	return (0);
+}
